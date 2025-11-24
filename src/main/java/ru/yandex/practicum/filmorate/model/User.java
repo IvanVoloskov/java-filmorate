b025489f2fbfe6812ca619da13c5f826abdc9b1b
Voloskov Ivan <voloskov.ivan03@mail.ru>
2025-11-24 11:41:33 +0300

    Сделать доп.задание для улучшения валидации проекта + исправить checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.model;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-import lombok.NonNull;
 
 import java.time.LocalDate;
 
